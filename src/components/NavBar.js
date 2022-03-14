8739d200841e11f90db0356357a74765e7dc687a
Randy Grullon <1089318@est.intec.edu.do>
2022-03-14 00:51:31 -0400

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className="navbar-container">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"/>
+    </div>
+  )
+}
+
+export default NavBar
+
+
