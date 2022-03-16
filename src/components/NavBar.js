6a5bd89c834cc60692a5c4bddbc646a5a2c8a1ad
RANDY EMMANUEL GRULLON BAEZ <randy.grullon@ikeasi.com>
2022-03-16 14:31:51 -0400

    s

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
 
     <div className="navbar-container">
     
-      <img
+      <img 
         className='navBar-Logo'
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
         alt="Netflix Logo" />
